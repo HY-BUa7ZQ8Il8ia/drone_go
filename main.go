db980265e5cb813beca4e662489e6d231d53fc47
SH <SH@example.com>
2020-05-22 12:52:54 +0900

    add drone manage system

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"gotello/app/controllers"
+	"gotello/config"
+	"gotello/utils"
+	"log"
+)
+
+func main() {
+	utils.LoggingSettings(config.Config.LogFile)
+	log.Println(controllers.StartWebServer)
+}
+
